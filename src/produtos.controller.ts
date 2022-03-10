0989fc3fd85b86c411deb5f08c299edbb136d417
Pedro Vinicius <pedro.santos@metasix.com.br>
2022-03-10 10:45:20 -0300

    Criando a produtos.controller

---
+++ src/produtos.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+
+@Controller('produtos')
+export class ProdutosController {
+
+    @Get()
+    obterTodos(): string {
+      return 'Lista de produtos';
+   }
+
+   @Get(':id')
+   obterUm(@Param() params) : string {
+       return `Retorna os dados do produto ${params.id}`;
+   }
+
+   @Post()
+   criar(@Body() produto): string {
+       console.log(produto);
+       return 'Produto criado com sucesso';
+   }
+
+   @Put()
+   alterar(@Body() produto): string {
+       console.log(produto);
+       return 'Produto alterado com sucesso';
+   }
+
+   @Delete(':id')
+   apagar(@Param() params): string {
+       return `Apaga o produto ${params.id}`;
+   }
+
+}
