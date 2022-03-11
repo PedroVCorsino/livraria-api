fcf7b3ea2d629158a24ed460c73c2f811cf506ea
Pedro Vinicius <pedro.santos@metasix.com.br>
2022-03-11 13:55:24 -0300

    Configurando parametros para banco de dados(postgresql) usando sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Livro } from './livro.model';
 import { LivrosController } from './livros.controller';
 import { LivroService } from './livros.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, LivrosController],
-  providers: [AppService, LivroService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pedro',
+      database: 'livraria',
+      autoLoadModels: true, // carrega automaticamente os models
+      synchronize: true, // sincroniza o banco de dados e cria as tabelas caso não existam
+    }),
+    SequelizeModule.forFeature([Livro]), // carrega os models
+  ],
+  controllers: [AppController, LivrosController], 
+  providers: [AppService, LivroService], 
 })
-export class AppModule {}
+export class AppModule {} 
