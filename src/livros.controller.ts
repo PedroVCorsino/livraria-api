8c0ea5b8c341dff02f97d654f33bbc884ca4eed3
Pedro Vinicius <pedro.santos@metasix.com.br>
2022-03-11 17:18:05 -0300

    Refatorando a controller para tornar os metodos async e retornar Promises

--- src/livros.controller.ts
+++ src/livros.controller.ts
@@ -8,27 +8,27 @@ export class LivrosController {
 constructor(private livrosService: LivroService) {} // injeção de dependência
 
     @Get()
-    obterTodos(): Livro[] {
+    async obterTodos(): Promise<Livro[]> {
       return this.livrosService.obterTodos();
    }
 
    @Get(':id') 
-   obterUm(@Param() params) : Livro {
+   async obterUm(@Param() params) : Promise<Livro> {
         return this.livrosService.obterUm(params.id);
    }
 
    @Post()
-   criar(@Body() livro: Livro) {
+   async criar(@Body() livro: Livro) { 
         this.livrosService.criar(livro);
    }
 
    @Put()
-   alterar(@Body() livro: Livro){
+   async alterar(@Body() livro: Livro){
        this.livrosService.alterar(livro);
    }
 
    @Delete(':id')
-   apagar(@Param() params) {
+   async apagar(@Param() params) {
        this.livrosService.apagar(params.id);
    }
 
